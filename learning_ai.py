3c61c2fd072d50fb169e27f01e371fcd2c6978fe
hihihi13 <novagaming0408@gmail.com>
2026-08-19 17:40:24 +0800

    Fix loop condition in while statement

--- learning_ai.py
+++ learning_ai.py
@@ -23,7 +23,7 @@ loop_count = 0
 
 while (
     time.time() - start_time) < max_runtime_seconds:
-        loop_count < max_loops_per_session
+        
     
     try:
         current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
